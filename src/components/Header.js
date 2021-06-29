b52c0b2d69c0f48726323d2806553ff164343d39
Kenny Nguyen <kenngu90@outlook.com>
2021-06-29 22:30:24 +0200

    'init'

---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData"
+
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORIX</NavLink>
+      <Bars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavLink to={item.link} key={index}>
+            {item.title}
+          </NavLink>
+        ))}
+      </NavMenu>
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: white;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
